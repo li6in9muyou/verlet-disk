9e0d4d33effd5dc40c770087e12305e0e3aae1b2
li6in9muyou <libq5520@mails.jlu.edu.cn>
2024-06-20 12:18:19 +0800

    fix: position offset

--- main.js
+++ main.js
@@ -32,12 +32,12 @@ class Verlet {
   }
 }
 
-function renderDisk(position) {
+function renderDisk(position, extra) {
   const disk = document.createElement("div");
   disk.classList.add("disk");
   // TODO: compensate for disk radius
-  disk.style.top = `${position.y}px`;
-  disk.style.left = `${position.x}px`;
+  disk.style.top = `${position.y - extra.size / 2}px`;
+  disk.style.left = `${position.x - extra.size / 2}px`;
   document.body.appendChild(disk);
   console.log("position", position);
 }
@@ -46,7 +46,7 @@ const VEC2_ZERO = { x: 0, y: 0 };
 const VEC2_GRAVITY = { x: 0, y: 10 };
 
 const dynamics = [
-  new Verlet({ x: 50, y: 0 }, { x: 50, y: 0 }, VEC2_GRAVITY),
+  new Verlet({ x: 20, y: 20 }, { x: 20, y: 20 }, VEC2_GRAVITY),
   new Verlet({ x: 300, y: 200 }, { x: 300, y: 200 }, VEC2_GRAVITY),
 ];
 
@@ -89,13 +89,14 @@ function updateOne(dt, verlet) {
   verlet.current_position = next_position;
 }
 
-dynamics.forEach((d) => renderDisk(d.current_position));
+const extra = [{ size: 40 }, { size: 40 }];
+dynamics.forEach((d, idx) => renderDisk(d.current_position, extra[idx]));
 
 const ITERATION_LIMIT = 100;
 let iteration = 0;
 function go() {
   dynamics.forEach((v) => updateOne(16, v));
-  dynamics.forEach((v) => renderDisk(v.current_position));
+  dynamics.forEach((v, idx) => renderDisk(v.current_position, extra[idx]));
   if (iteration < ITERATION_LIMIT) {
     iteration += 1;
     requestAnimationFrame(go);
